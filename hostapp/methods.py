7a09f14e76e82e48a3e82913cecdf43b07a5f2b5
Tech7ie <madexlol6661@gmail.com>
2023-12-10 05:55:43 +1000

    methods fast fix

--- hostapp/methods.py
+++ hostapp/methods.py
@@ -6,25 +6,30 @@ class DB:
     def selectIdByCode(code):
         with connection.cursor() as cursor:
             cursor.execute("SELECT id FROM links WHERE code = %s", [code])
-            raw = cursor.fetchone()[0]
-        return raw #должно айди
+            code_id = cursor.fetchone()[0]
+        return code_id
+
 
     @staticmethod
     def selectBoxes(code):
         code_id = DB.selectIdByCode(code)
+        print(code_id)
         with connection.cursor() as cursor:
             cursor.execute("SELECT link FROM box WHERE code_id = %s", [code_id])
             raw = cursor.fetchall()
         return raw
+
+
     @staticmethod
     def createCode(code):
         with connection.cursor() as cursor:
             cursor.execute("INSERT INTO links (user_id, code) VALUES (%s, %s)", [1, str(code)])
             connection.commit()
 
+
     @staticmethod
     def addLink(link, code):
-        code_id = 1#selectIdByCode(code)
+        code_id = selectIdByCode(code)
         with connection.cursor() as cursor:
 	        cursor.execute("INSERT INTO box (link, code_id) VALUES (%s, %s)", [link, code_id])
 	        connection.commit()
